b67e9d45c749c4eea7352796b0fedf6298dc1a7c
Arthur Mayer <info@citex.net>
2015-01-20 19:30:24 +0100

    Update 15_named_nameservers.pl

--- 15_named_nameservers.pl
+++ 15_named_nameservers.pl
@@ -1,3 +1,24 @@
+# i-MSCP Listener::Secondarydns::Zonetransfer listener file
+# Copyright (C) 2015 UncleJ, Arthur Mayer <mayer.arthur@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+#
+## i-MSCP listener file modifies the zone files, removes the default nameservers and adds custom out-of-zone nameservers
+#
+
 package Listener::Named::Nameservers;
 
 use iMSCP::EventManager;
